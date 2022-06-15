d1445198f6eb57784e737c0b8f51f8cc558c8d01
Thiago Galhardo <thiago.galhardo@hotmail.com>
2022-06-14 22:43:18 -0300

    dompdf

--- modulo02/aula06/index.php
+++ modulo02/aula06/index.php
@@ -11,10 +11,25 @@ ini_set('display_errors', 1);
 
 include 'vendor/autoload.php';
 
-use Classes\Config\Usuario;
 
-// $us1 = new Classes\Usuario();
-$us2 = new Usuario();
+use Dompdf\Dompdf;
 
-// var_dump($us1);
-var_dump($us2);
+
+
+
+
+// reference the Dompdf namespace
+
+
+// instantiate and use the dompdf class
+$dompdf = new Dompdf();
+$dompdf->loadHtml('hello world');
+
+// (Optional) Setup the paper size and orientation
+$dompdf->setPaper('A4', 'landscape');
+
+// Render the HTML as PDF
+$dompdf->render();
+
+// Output the generated PDF to Browser
+$dompdf->stream();
